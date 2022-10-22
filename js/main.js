bbff48136c5ab40f9756bef7d0eeba7d3b9627fc
EzequielJara <ezequierjaraepet20@gmail.com>
2022-10-21 23:07:54 -0300

    Cambiando clases

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const main_img = document.querySelector (` .main_img `)
-const img_select = document.querySelectorAll (` .img_select `)
+const thumbnails = document.querySelectorAll (` .thumbnails `)
 const activo = document.querySelector (`.activo `)
 
-img_select.forEach(selec => {
-    selec.addEventListener(`click`, function(){
+thumbnails.forEach(thumb => {
+    thumb.addEventListener(`click`, function(){
         this.classList.addEventListener(`activo`)
     })
 })
