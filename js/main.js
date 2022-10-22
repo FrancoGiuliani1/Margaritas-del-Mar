163b1f8d42fca84cfc9aded59e22b76d2d24edea
EzequielJara <ezequierjaraepet20@gmail.com>
2022-10-21 23:00:48 -0300

    Añadiendo el forEach de las imagenes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ const main_img = document.querySelector (` .main_img `)
 const img_select = document.querySelectorAll (` .img_select `)
 const activo = document.querySelector (`.activo `)
 
+img_select.forEach(selec => {
+
+})
