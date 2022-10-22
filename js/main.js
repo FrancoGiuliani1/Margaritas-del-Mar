7961a8e53c36c45b065d12439fa6512775442da8
EzequielJara <ezequierjaraepet20@gmail.com>
2022-10-21 23:03:21 -0300

    Agregando la funcion activo cuando sea click

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ const img_select = document.querySelectorAll (` .img_select `)
 const activo = document.querySelector (`.activo `)
 
 img_select.forEach(selec => {
-
+    selec.addEventListener(`click`, function(){
+        this.classList.addEventListener(`activo`)
+    })
 })
