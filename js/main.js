746e384020acf4c3cba7109b89797798c1f20438
EzequielJara <ezequierjaraepet20@gmail.com>
2022-10-21 23:56:08 -0300

    Duplicando los archivos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const main_img = document.querySelector ( '.main_img ')
 const img_select = document.querySelectorAll (' .img_select ')
-img_select.forEach(thumb => {
-    thumb.addEventListener('click', function(){
+img_select.forEach(selec => {
+    selec.addEventListener('click', function(){
         const active = document.querySelector ('.active ')
         active.classList.remove('active')
         this.classList.add('active')
