4fd31d992dfe12c321fe80762cba71187a864134
EzequielJara <ezequierjaraepet20@gmail.com>
2022-10-21 23:12:36 -0300

    Cambiando de lugar las clases

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const main_img = document.querySelector (` .main_img `)
-const thumbnails = document.querySelectorAll (` .thumbnails `)
-const activo = document.querySelector (`.activo `)
+const main_img = document.querySelector (" .main_img ")
+const thumbnails = document.querySelectorAll (" .thumbnails ")
+const activo = document.querySelector (".activo ")
 
 thumbnails.forEach(thumb => {
-    thumb.addEventListener(`click`, function(){
-        this.classList.addEventListener(`activo`)
+    thumb.addEventListener("click", function(){
+        this.classList.add("activo")
     })
 })
