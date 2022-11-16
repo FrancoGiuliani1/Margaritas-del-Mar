a8ecae99290e2b9f4cdd4e1d7fb25e731eae6482
Franco <you@example.com>
2022-11-16 18:35:05 -0300

    Creo la variable para carousel

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded',() => {
 
-    const elementosCarousel = document.querySelectorAll('.carousel');
+const elementosCarousel = document.querySelectorAll('.carousel');
+M.Carousel.init(elementosCarousel, {
+
+});
+
 });
