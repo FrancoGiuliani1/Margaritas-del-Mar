2eafd498689251498ce6a50c481cbf9920acabfa
Franco <you@example.com>
2022-11-16 18:37:12 -0300

    indico una transicion

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded',() => {
 
 const elementosCarousel = document.querySelectorAll('.carousel');
 M.Carousel.init(elementosCarousel, {
-
+duration:150,
+dist: -80,
 });
 
 });
